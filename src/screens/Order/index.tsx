0fe2fc5d9eaff38334d09e87e34be0e4a5fa4624
Wallace Júnior <wflj1997@hotmail.com>
2022-07-25 15:52:53 -0300

    Imagem do pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,10 +1,16 @@
 import { BackButton } from "~/components/BackButton";
-import { Container, Header } from "./styles";
+import { Container, Header, Photo } from "./styles";
 
 export const Order: React.FC = () => (
   <Container behavior="position" enabled>
     <Header>
       <BackButton />
     </Header>
+
+    <Photo
+      source={{
+        uri: "https://firebasestorage.googleapis.com/v0/b/gopizza-744b6.appspot.com/o/pizzas%2F1658774286076.png?alt=media&token=6dde05c5-b294-4dcd-9951-d26960bfdef1",
+      }}
+    />
   </Container>
 );
--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -12,8 +12,13 @@ export const Header = styled(LinearGradient).attrs(({ theme }) => {
   };
 })`
   width: 100%;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
   padding: ${getStatusBarHeight() + 33}px 24px 108px;
+`;
+
+export const Photo = styled.Image`
+  width: 240px;
+  height: 240px;
+  border-radius: 120px;
+  align-self: center;
+  margin-top: -120px;
 `;
