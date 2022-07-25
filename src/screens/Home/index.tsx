da74224279b69def82111d2b0510c828da56c942
Wallace Júnior <wflj1997@hotmail.com>
2022-07-25 13:00:30 -0300

    Header da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,30 @@
-import { Container } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "styled-components/native";
+import happyEmoji from "~/assets/happy.png";
+import {
+  Container,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+  Header,
+  SignOutButton,
+} from "./styles";
 
-export const Home: React.FC = () => <Container></Container>;
+export const Home: React.FC = () => {
+  const { colors } = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
+
+        <SignOutButton>
+          <MaterialIcons name="logout" size={24} color={colors.title} />
+        </SignOutButton>
+      </Header>
+    </Container>
+  );
+};
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,46 @@
-import styled from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => {
+  return {
+    colors: theme.colors.gradient,
+  };
+})`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${getStatusBarHeight() + 33}px 24px 58px;
+`;
+
+export const Greeting = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const GreetingEmoji = styled.Image`
+  width: 32px;
+  height: 32px;
+  margin-right: 12px;
+`;
+
+export const GreetingText = styled.Text`
+  font-size: 20px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.title};
+    color: ${theme.colors.title};
+  `}
+`;
+
+export const SignOutButton = styled(TouchableOpacity)`
+  justify-content: center;
+  align-items: center;
 `;
