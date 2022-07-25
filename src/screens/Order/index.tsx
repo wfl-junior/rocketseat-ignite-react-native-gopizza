7dc3afe1ae30205984d0cb97f5fa7d8300f8a9eb
Wallace Júnior <wflj1997@hotmail.com>
2022-07-25 16:26:32 -0300

    Formulário de pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,34 +1,72 @@
 import { useState } from "react";
+import { ScrollView } from "react-native";
 import { BackButton } from "~/components/BackButton";
+import { Button } from "~/components/Button";
+import { Input } from "~/components/Input";
 import { RadioButton } from "~/components/RadioButton";
 import { PIZZA_SIZES } from "~/utils/pizzaSizes";
-import { Container, Header, Photo, Sizes } from "./styles";
+import {
+  Container,
+  Content,
+  Form,
+  FormRow,
+  Header,
+  InputGroup,
+  Label,
+  Photo,
+  Price,
+  Sizes,
+  Title,
+} from "./styles";
 
 export const Order: React.FC = () => {
   const [selectedSize, setSelectedSize] = useState("M");
 
   return (
     <Container behavior="position" enabled>
-      <Header>
-        <BackButton />
-      </Header>
+      <Content>
+        <Header>
+          <BackButton />
+        </Header>
 
-      <Photo
-        source={{
-          uri: "https://firebasestorage.googleapis.com/v0/b/gopizza-744b6.appspot.com/o/pizzas%2F1658774286076.png?alt=media&token=6dde05c5-b294-4dcd-9951-d26960bfdef1",
-        }}
-      />
+        <Photo
+          source={{
+            uri: "https://firebasestorage.googleapis.com/v0/b/gopizza-744b6.appspot.com/o/pizzas%2F1658774286076.png?alt=media&token=6dde05c5-b294-4dcd-9951-d26960bfdef1",
+          }}
+        />
 
-      <Sizes>
-        {PIZZA_SIZES.map(size => (
-          <RadioButton
-            key={size.id}
-            title={size.title}
-            selected={selectedSize === size.id}
-            onPress={() => setSelectedSize(size.id)}
-          />
-        ))}
-      </Sizes>
+        <Form>
+          <Title>Gauchesca</Title>
+          <Label>Selecione um tamanho</Label>
+
+          <Sizes>
+            {PIZZA_SIZES.map(size => (
+              <RadioButton
+                key={size.id}
+                title={size.title}
+                selected={selectedSize === size.id}
+                onPress={() => setSelectedSize(size.id)}
+              />
+            ))}
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Número da mesa</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+
+          <Price>Total: R$10,00</Price>
+
+          <Button title="Confirmar pedido" />
+        </Form>
+      </Content>
     </Container>
   );
 };
--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -1,9 +1,15 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled.KeyboardAvoidingView`
+export const Container = styled.View`
   flex: 1;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled(LinearGradient).attrs(({ theme }) => {
@@ -23,10 +29,57 @@ export const Photo = styled.Image`
   margin-top: -120px;
 `;
 
+export const Form = styled.View`
+  width: 100%;
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  margin-bottom: 46px;
+  text-align: center;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.title};
+    color: ${theme.colors.secondary[900]};
+  `}
+`;
+
+export const Label = styled.Text`
+  font-size: 14px;
+  margin-bottom: 16px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.text};
+    color: ${theme.colors.secondary[900]};
+  `}
+`;
+
 export const Sizes = styled.View`
   width: 100%;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   margin-bottom: 40px;
+`;
+
+export const FormRow = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputGroup = styled.View`
+  width: 48%;
+`;
+
+export const Price = styled.Text`
+  font-size: 14px;
+  margin: 24px 0;
+  align-self: flex-end;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.text};
+    color: ${theme.colors.secondary[900]};
+  `}
 `;
